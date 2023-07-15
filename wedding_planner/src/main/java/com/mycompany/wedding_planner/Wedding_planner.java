28b9f74a48fcc1fafdcac4a27be85e6f83fc95ca
shobanav09 <114589593+shobanav09@users.noreply.github.com>
2023-07-15 18:56:53 +0530

    Add files via upload

---
+++ wedding_planner/src/main/java/com/mycompany/wedding_planner/Wedding_planner.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.wedding_planner;
+/**
+ *
+ * @author Shobs
+ */
+public class Wedding_planner {
+    public static void main(String[] args) {
+       Home jf1=new Home();
+        jf1.show();
+    }
+}
